941eb3e0264de1d0c6984e7b2b2214fb55dacd0a
bke82024 <bke82024@gmail.com>
2024-11-28 14:23:08 +0100

    sent person to uni

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ console.log(people[0].firstName + " " + people[0]["lastName"]);
 //3. That was tiresome.. just so much typing. Let's write a method to that we never need to that again..
 //create a method in every person object that returns first and last name, call it fullName. This can be done manually for each one or with a loop.
 //Print fullName of the second person in the array by calling the method.
+
 console.log(people[1].fullName);
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
@@ -69,18 +70,47 @@ function birthday(person) {
   console.table(person);
 }
 birthday(people[2]);
+
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //5. Person three is throwing a giant party! create a function called fotballPubben(). The function should check if the person is over 18, print "party time" if they are and "too young" if they are not. It should also print the name of the person.
 // use a loop to call the function for every person in the array.
-function fotballPubben() {
-  if  
+
+function fotballPubben(person) {
+  if (person.age >= 18) {
+    console.log(person.fullName + ": party time!");
+  } else {
+    console.log(person.fullName + ": too young!");
+  }
 }
+
+people.forEach((person) => {
+  fotballPubben(person);
+});
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //6. It's time for school! Create a function called university. It should take in an person object as well as type of degree (bachelors or masters) as arguments.
 // The function should update age and add two properties called "degree" and "student loan". The value of age, degree and student loan should change depending on what degree
 //was passed into the function. Send one person to uni and print the result.
+
+function university(person, degreeType) {
+  if (degreeType === "bachelors") {
+    person.age += 3;
+    person.degree = "Bachelors";
+    person.studentLoan = 30000;
+  } else if (degreeType === "masters") {
+    person.age += 5;
+    person.degree = "Masters";
+    person.studentLoan = 50000;
+  } else {
+    console.log("Invalid degree type provided");
+    return;
+  }
+}
+
+university(people[0], "bachelors");
+console.table(people[0]);
+
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 // 7. API TIME!

05d26eeacbdc522a2f44b209a2902b591c1ba101
bke82024 <bke82024@gmail.com>
2024-11-28 12:32:48 +0100

    started task 5

--- main.js
+++ main.js
@@ -6,30 +6,45 @@ const people = [
     lastName: "Birknes",
     age: 28,
     job: false,
-  },
-  {
-    firstName: "Lars",
-    lastName: "Krogsrud",
-    age: 12,
-    job: true,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
   },
   {
     firstName: "Emil",
     lastName: "Endresen",
     age: 35,
     job: true,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+  {
+    firstName: "Lars",
+    lastName: "Krogsrud",
+    age: 17,
+    job: true,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
   },
   {
     firstName: "Kai",
     lastName: "Midget",
     age: 9999999999999999999999999999999999999,
     job: true,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
   },
   {
     firstName: "Fadi",
     lastName: "Hadi",
     age: 27,
     job: false,
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
   },
 ];
 
@@ -43,14 +58,24 @@ console.log(people[0].firstName + " " + people[0]["lastName"]);
 //3. That was tiresome.. just so much typing. Let's write a method to that we never need to that again..
 //create a method in every person object that returns first and last name, call it fullName. This can be done manually for each one or with a loop.
 //Print fullName of the second person in the array by calling the method.
+console.log(people[1].fullName);
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //4. It's the third person's birthday! And their job status changed. Update their age and job status using dot notation.
+function birthday(person) {
+  person.job ? (person.job = false) : (person.job = true);
+  person.age++;
+  console.table(person);
+}
+birthday(people[2]);
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //5. Person three is throwing a giant party! create a function called fotballPubben(). The function should check if the person is over 18, print "party time" if they are and "too young" if they are not. It should also print the name of the person.
 // use a loop to call the function for every person in the array.
+function fotballPubben() {
+  if  
+}
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //6. It's time for school! Create a function called university. It should take in an person object as well as type of degree (bachelors or masters) as arguments.
